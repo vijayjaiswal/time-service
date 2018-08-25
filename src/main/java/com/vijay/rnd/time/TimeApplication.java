b8f1c85f440e11bfde3e9f90268ccf7594be4af1
Vijay J <vijay.jaiswal@qio.io>
2018-08-25 18:52:42 +0530

    Added methods for TimeZone

--- src/main/java/com/vijay/rnd/time/TimeApplication.java
+++ src/main/java/com/vijay/rnd/time/TimeApplication.java
@@ -2,7 +2,9 @@ package com.vijay.rnd.time;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+ * Author: Vijay Jaiswal (victory_ja100@yahoo.com) 
+ */
 @SpringBootApplication
 public class TimeApplication {
 	public static void main(String[] args) {
--- src/main/java/com/vijay/rnd/time/controller/TimeController.java
+++ src/main/java/com/vijay/rnd/time/controller/TimeController.java
@@ -1,13 +1,33 @@
 package com.vijay.rnd.time.controller;
 
-import org.joda.time.DateTime;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Set;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+/*
+ * Author: Vijay Jaiswal (victory_ja100@yahoo.com) 
+ */
 @RestController
 public class TimeController {
 	 @RequestMapping("/now")
 	 public DateTime getNow(){
 		 return new DateTime();
 	 }
+
+	 @RequestMapping("/timeZonesIDs")
+	 public Set<String> getTimeZonesIDs(){
+		 return DateTimeZone.getAvailableIDs();
+	 }
+	 
+	 @RequestMapping("/timeForZonesIDs")
+	 public DateTime getTimeForZonesIDs(@RequestParam String timeZonesID){
+		 if(timeZonesID!=null){
+			 return new DateTime().withZone(DateTimeZone.forID(timeZonesID));
+		 }
+		 return new DateTime().withZone(DateTimeZone.getDefault());
+	 }
+	 
 }
